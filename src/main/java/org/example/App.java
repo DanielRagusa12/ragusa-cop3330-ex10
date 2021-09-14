584eac1c3e74c4c85a5f2c5ed3d557c9cd62c58d
DanielRagusa12 <daniel.ragusa129@gmail.com>
2021-09-13 22:59:19 -0400

    Done

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,14 @@
 package org.example;
 
 /*
- *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  UCF COP3330 Fall 2021 Assignment 10 Solution
  *  Copyright 2021 Daniel Ragusa
  */
 
 import java.util.Scanner;
-import java.util.Calendar;
 
 public class App
 {
-    public static final double gallon=350;
     public static void main( String[] args )
     {
         int price1;
@@ -19,31 +17,25 @@ public class App
         int quantity1;
         int quantity2;
         int quantity3;
-
+        Scanner input=new Scanner(System.in);
 
         System.out.print("Enter the price of item 1: ");
-        Scanner input=new Scanner(System.in);
         price1=input.nextInt();
 
         System.out.print("Enter the quantity of item 1: ");
-        Scanner input2=new Scanner(System.in);
-        quantity1=input2.nextInt();
+        quantity1=input.nextInt();
 
         System.out.print("Enter the price of item 2: ");
-        Scanner input3=new Scanner(System.in);
-        price2=input3.nextInt();
+        price2=input.nextInt();
 
         System.out.print("Enter the quantity of item 2: ");
-        Scanner input4=new Scanner(System.in);
-        quantity2=input4.nextInt();
+        quantity2=input.nextInt();
 
         System.out.print("Enter the price of item 3: ");
-        Scanner input5=new Scanner(System.in);
-        price3=input5.nextInt();
+        price3=input.nextInt();
 
         System.out.print("Enter the quantity of item 3: ");
-        Scanner input6=new Scanner(System.in);
-        quantity3=input6.nextInt();
+        quantity3=input.nextInt();
 
         int totalPrice1=price1*quantity1;
         int totalPrice2=price2*quantity2;
